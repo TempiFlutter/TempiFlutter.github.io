8d813f29b1caf65bdcf96bdc62aa7cb99122564e
TempiFlutter <151827650+TempiFlutter@users.noreply.github.com>
2024-03-27 16:05:56 +0500

    27 March

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -36,9 +36,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "95dd51f13701c668f8d2de65b9bd1ef3
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "afc33071dcac92620dfc0a4dba08767b",
-"/": "afc33071dcac92620dfc0a4dba08767b",
-"main.dart.js": "0692459bdf6189cc4a9441ac61d312dc",
+"index.html": "e040d0279f1cf05a2d383e7cd4d2c830",
+"/": "e040d0279f1cf05a2d383e7cd4d2c830",
+"main.dart.js": "ebca8cb710168be9ddf62ee7d633cd77",
 "manifest.json": "c7ab9d615321285a5cc686c4790f8244",
 "version.json": "ac41d82c9541ce3c85ce7c3cf8b371e7"};
 // The application shell files that are downloaded before a service worker can
