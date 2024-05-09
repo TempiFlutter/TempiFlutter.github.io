b749940f6fbeb4d61a742549f95ea89e5b1725de
TempiFlutter <151827650+TempiFlutter@users.noreply.github.com>
2024-05-09 15:28:09 +0500

    9 May Build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "ac41d82c9541ce3c85ce7c3cf8b371e7",
-"index.html": "bf12076fe6e4deb3da721758695efed9",
-"/": "bf12076fe6e4deb3da721758695efed9",
-"main.dart.js": "50c38b83a1082027d38a88af71ea1042",
+"index.html": "8975d73710ab82bb91e0b3bc7424e85c",
+"/": "8975d73710ab82bb91e0b3bc7424e85c",
+"main.dart.js": "2d0bfbd72ae80b3b99d79c527ad174a8",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
